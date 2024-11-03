4a38675e3415496340950a0b6608c51c15b71a81
Amgad Abdullah <amga200d@gmail.com>
2024-11-02 20:07:48 -0600

    find key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,36 @@
+//Assertion function
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// findKeyByValue
+
+const findKeyByValue = function(obj, val) {
+  for (const key in obj) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+};
+
+
+
+//Test Cases
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+
+
