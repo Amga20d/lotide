d1949a130096801463e17c093cef43f25a3b0f02
Amgad Abdullah <amga200d@gmail.com>
2024-11-02 19:46:40 -0600

    a function that returns the indices of letters in a sentence

---
+++ letterPositions.js
@@ -0,0 +1,51 @@
+// Assertion function
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+    
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// letterPositions function
+
+const letterPositions = function(sentence) {
+  const result = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (result[sentence[i]]) {
+        result[sentence[i]].push(i);
+      } else {
+        result[sentence[i]] = [i];
+      }
+    }
+  }
+  return result;
+};
+
+
+// Test Cases
+
+const result1 = letterPositions("hello");
+
+
+assertArraysEqual(result1["h"], [0]);
+assertArraysEqual(result1["e"], [1]);
+assertArraysEqual(result1["l"], [2, 3]);
+assertArraysEqual(result1["o"], [4]);
+
+
